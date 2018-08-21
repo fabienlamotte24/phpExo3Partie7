3961debabc6e87fadc0e889abf25862927696217
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-08-21 14:11:45 +0200

    Correction avec la classe

--- index.php
+++ index.php
@@ -6,15 +6,11 @@
     <title>Exercice 3</title>
   </head>
   <body>
-    <!--Création d'un formulaire simple avec get en méthode
-  //Condition pour gérer le cas où le formulaire n'est pas bien rempli
-//On accède à la requète lorsque toutes les conditions sont remplies => On passe à la page suivante
-//Ou on reste sur la page du formulaire avec les messages d'erreur pour que l'utilisateur renseigne les bons caractères-->
 <form class="form" action="user.php" method="get">
  <!--Création d'un formulaire simple avec get en méthode-->
-  <label for="lastName">Votre nom: </label><input type="text" name="lastName" maxlength="15" size="10" placeholder="nom" /><br><br>
-  <label for="firstName">Votre prénom: </label><input type="text" name="firstName" maxlength="10" size="10" placeholder="prénom" /><br><br>
-  <input type="submit" name="submit" />
+  <label for="lastName">Votre nom: </label><input type="text" name="lastName"  id="lastName" maxlength="15" size="10" placeholder="nom" />
+  <label for="firstName">Votre prénom: </label><input type="text" name="firstName"  id="firstName" maxlength="15" size="10" placeholder="prénom" />
+  <input type="submit" Value="Page Suivante" />
 </form>
   </body>
 </html>
--- user.php
+++ user.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8" />
-    <title>Exercice 3</title>
-  </head>
-  <body>
-    <p>
-<?php
-if(isset($_GET['lastName']) && isset($_GET['firstName'])){
-  echo $_GET['lastName'] . ' ' . $_GET['firstName'];
-}
-?>
-    </p>
-  </body>
+<head>
+  <meta charset="utf-8" />
+  <title>Exercice 3</title>
+</head>
+<body>
+  <p><!--Utilisation d'une condition avec ISSET pour vérifier l'existence des données enregistrée par la page précédentes
+    Puis affichage de la phrase avec les données voulues-->
+    <?php
+      if(isset($_GET['lastName']) && isset($_GET['firstName'])){
+        echo 'Mon prénom est <b>' . $_GET['firstName'] . '</b> et mon nom de famille <b> ' . $_GET['lastName'] . '</b>';
+      }
+    ?>
+  </p>
+</body>
 </html>
